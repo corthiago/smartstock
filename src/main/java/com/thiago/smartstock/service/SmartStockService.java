bf9cdb4b646c09f15229fcb5b5dff0539ad4bc40
Thiago Correia <cor.thiago@gmail.com>
2026-08-03 17:24:30 -0300

    feat: add reorder quantity calculation

--- src/main/java/com/thiago/smartstock/service/SmartStockService.java
+++ src/main/java/com/thiago/smartstock/service/SmartStockService.java
@@ -1,5 +1,6 @@
 package com.thiago.smartstock.service;
 
+import com.thiago.smartstock.domain.CsvStockItem;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,14 +19,32 @@ public class SmartStockService {
         // 1. read csv file
         try {
             var items = reportService.readStockReport(reportPath);
+
+            items.forEach(item -> {
+
+                if(item.getQuantity() < item.getReorderThreshold()){
+                    // 2. call purchasing department api for each stock item
+                    var reorderQuantity = calculateReorderQuantity(item);
+
+                    // 3. persist items at mongodb
+
+
+                }
+            });
+
+
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        // 2. call purchasing department api for each stock item
 
+    }
 
-        // 3.
+    private Integer calculateReorderQuantity(CsvStockItem item) {
+
+        return  item.getReorderThreshold() + ((int) Math.ceil(item.getReorderThreshold() * 0.2));
 
     }
 

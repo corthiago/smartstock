80f7d7d8ee3896a07b8f20ab797a99e5c20150b9
Thiago Correia <cor.thiago@gmail.com>
2026-08-04 16:43:00 -0300

    feat: add environment variables

--- src/main/java/com/thiago/smartstock/SmartstockApplication.java
+++ src/main/java/com/thiago/smartstock/SmartstockApplication.java
@@ -1,6 +1,7 @@
 package com.thiago.smartstock;
 
 import com.thiago.smartstock.client.GoogleClient;
+import com.thiago.smartstock.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,17 +10,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class SmartstockApplication implements CommandLineRunner {
+public class SmartstockApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartstockApplication.class, args);
 	}
 
-    @Autowired
-    private GoogleClient googleClient;
-
-    @Override
-    public void run(String... args) throws Exception {
-        googleClient.helloGoogle();
-    }
 }
